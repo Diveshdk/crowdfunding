fa994582c19b6a18730035e15e40239a7d23f588
Diveshdk <kankanifivesh6@gmail.com>
2025-06-28 17:45:58 +0530

    first commit

---
+++ Context/contants.js
@@ -0,0 +1,4 @@
+import crowdFunding from "./CrowdFunding.json";
+
+export const CrowdFundingAddress = "0xb53915cf1ccb67c545d1864bceb55ae9a38712fd";
+export const CrowdFundingABI = crowdFunding.abi;
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
 };
 
 export default nextConfig;
